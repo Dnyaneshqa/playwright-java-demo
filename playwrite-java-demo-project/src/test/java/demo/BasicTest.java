9d20bd9c5ebe6a1fa574c43013ce306172e7b65d
Dnyaneshwar Rathod <dnyaneshwarqatech9@gmail.com>
2025-09-28 17:34:29 +0530

    Update BasicTest.java

--- playwrite-java-demo-project/src/test/java/demo/BasicTest.java
+++ playwrite-java-demo-project/src/test/java/demo/BasicTest.java
@@ -24,8 +24,19 @@ public class BasicTest {
         String title = page.title();
         System.out.println("Current title of page: " +title);
         Assert.assertTrue(title.contains("Playwright"), "Title should contain 'Playwright'");
+        System.out
     }
 
+    @Test
+    public void testDocsLink() {
+        page.navigate("https://playwright.dev/");
+        page.click("a:get-by-text('Docs')");
+        String url = page.url();
+        System.out.println("Navigated URL: " + url);
+        Assert.assertTrue(url.contains("docs"), "Should navigate to docs page");
+    }
+
+    
     @AfterClass
     public void teardown() {
         browser.close();
